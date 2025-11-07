5194c8fc0234487b969142ee7455ba7276f74633
Rayen467 <133085728+Rayen467@users.noreply.github.com>
2025-11-07 16:17:11 +0700

    saya telah membuat splash screen2

---
+++ lib/srceens/splash_screen2.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+
+class SplashScreen2 extends StatelessWidget {
+  const SplashScreen2({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Padding(
+        padding: const EdgeInsets.all(24.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Image.asset('assets/images/image2.png', height: 300),
+            const SizedBox(height: 30),
+            const Text(
+              'Welcome',
+              style: TextStyle(fontSize: 28, fontWeight: FontWeight.bold),
+            ),
+            const SizedBox(height: 10),
+            const Text(
+              "Don't worry we got you cover.\nUse Walle instead of cash!",
+              textAlign: TextAlign.center,
+              style: TextStyle(fontSize: 16, color: Colors.grey),
+            ),
+            const SizedBox(height: 40),
+            ElevatedButton(
+              onPressed: () {},
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Colors.teal,
+                minimumSize: const Size.fromHeight(50),
+              ),
+              child: const Text('Continue'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
